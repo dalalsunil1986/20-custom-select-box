d70fc469ea51e2576eb0cb0ef93bf64213df7624
Mark Tellez <mark.tellez@icloud.com>
2018-05-21 01:49:38 -0500

    done

--- src/features/select-box/index.js
+++ src/features/select-box/index.js
@@ -1,12 +1,32 @@
 import React from 'react'
 
+import './styles.css'
+
 class SelectBox extends React.Component {
   state = {
-    items: this.props.items || []
+    items: this.props.items || [],
+    showItems: false,
   }
+
+  dropDown = () => {
+    this.setState(prevState => ({
+      showItems: !prevState.showItems
+    }))
+  }
+
   render() {
     return <div>
-      <div>
+      <div
+        className="select-box--arrow"
+        onClick={this.dropDown}
+      >
+        <span
+          className={`${this.state.showItems ? 'select-box--arrow-up' : 'select-box--arrow-down'}`}
+        />
+      </div>
+      <div
+        style={{display: this.state.showItems ? 'block' : 'none'}}
+      >
         {
           this.state.items.map(item => <div>
             { item.value }
