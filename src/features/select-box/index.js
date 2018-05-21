785fd2f0df30b3d80e21cdc4df435efee84cbe76
Mark Tellez <mark.tellez@icloud.com>
2018-05-21 01:53:55 -0500

    done

--- src/features/select-box/index.js
+++ src/features/select-box/index.js
@@ -6,6 +6,7 @@ class SelectBox extends React.Component {
   state = {
     items: this.props.items || [],
     showItems: false,
+    selectedItem: this.props.items[0] || this.props.selectedItem,
   }
 
   dropDown = () => {
@@ -14,25 +15,41 @@ class SelectBox extends React.Component {
     }))
   }
 
+  selectItem = (item) => {
+    this.setState({
+      selectedItem: item,
+      showItems: false,
+    })
+  }
+
   render() {
     return <div>
-      <div
-        className="select-box--arrow"
-        onClick={this.dropDown}
-      >
-        <span
-          className={`${this.state.showItems ? 'select-box--arrow-up' : 'select-box--arrow-down'}`}
-        />
+      <div className="select-box--box">
+        <div className="select-box--container">
+          <div className="select-box--selected-item">
+            { this.state.selectedItem.value }
+          </div>
+          <div
+            className="select-box--arrow"
+            onClick={this.dropDown}
+          ><span className={`${this.state.showItems ? 'select-box--arrow-up' : 'select-box--arrow-down'}`}/></div>
+        </div>
+        <div
+          className="select-box--items"
+          style={{display: this.state.showItems ? 'block' : 'none'}}
+        >
+          {
+            this.state.items.map(item => <div
+              key={item.id}
+              onClick={() => this.selectItem(item)}
+              className={this.state.selectedItem === item ? 'selected' : ''}
+            >
+              { item.value }
+            </div>)
+          }
+        </div>
       </div>
-      <div
-        style={{display: this.state.showItems ? 'block' : 'none'}}
-      >
-        {
-          this.state.items.map(item => <div>
-            { item.value }
-          </div>)
-        }
-      </div>
+      <input type="hidden" name={this.state.name} value={this.state.selectedItem.id} />
     </div>
   }
 }
