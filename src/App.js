fd418324274f53e92deb42054a0b9367fecb822c
Mark Tellez <mark.tellez@icloud.com>
2018-05-21 01:36:06 -0500

    cleaned

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react'
-import SelectBox from './features/select-box'
 
 class App extends Component {
   render() {
     return <div>
       <h1>Custom Select Box using ReactJS</h1>
       <div style={{margin: '16px', position: 'relative'}}>
-        <SelectBox
-          name="venue[country_id]"
-          items={[
-            { value: 'United States', id: 1 },
-            { value: 'Great Britian', id: 21 },
-            { value: 'Mexico', id: 43 },
-            { value: 'Canada', id: 2 },
-          ]}
-        />
+        
       </div>
     </div>
   }
